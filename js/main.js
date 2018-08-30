14989ea171e350785ce7ecf3e5a738d7f0962bb9
lychees <lychees67@gmail.com>
2018-08-30 22:16:28 +0800

    Revert "fix"
    This reverts commit 5b6acc7e57d6ddb572bb41e0fd86768c9f58271b.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ app = new Vue({
         identity: null,
         user_hpy_balance: 0,
         user_eos_balance: 0,
-        eos_net: null,
         last_bet: null,
         bet_input: "1.0000",
         bet_result: null,
@@ -257,9 +256,9 @@ app = new Vue({
         setIdentity: function (identity) {
             this.account = identity.accounts.find(acc => acc.blockchain === 'eos');
             this.identity = identity;
-            this.eos = scatter.eos(this.eos_net, Eos, {});
+            this.eos = scatter.eos(network, Eos, {});
             this.requiredFields = {
-                accounts: [this.eos_net]
+                accounts: [network]
             };
             this.get_current_balance();
         },
@@ -282,11 +281,7 @@ app = new Vue({
                             }
                         })
                         .catch(err => {
-                            // alert("Scatter 初始化失败.");
-                            if ('host_list' in network) {
-                                network.host_list[0];
-                                network.host_list.splice(0, 1);
-                            }
+                            alert("Scatter 初始化失败.");
                         });
                 }
             } else {
